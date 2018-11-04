5c03e2d2b5943b6a780fae231efc5999ff7c9e9d
starlord0302 <starlord0302@users.noreply.github.com>
2018-11-04 23:44:54 +0100

    BookingController updated with http methods.

--- cinema-booking/src/main/java/hu/elte/cinemabooking/controllers/BookingController.java
+++ cinema-booking/src/main/java/hu/elte/cinemabooking/controllers/BookingController.java
@@ -1,16 +1,26 @@
 package hu.elte.cinemabooking.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.elte.cinemabooking.entities.Booking;
+import hu.elte.cinemabooking.entities.ReservedSeat;
+import hu.elte.cinemabooking.entities.User;
 import hu.elte.cinemabooking.repositories.BookingRepository;
+import hu.elte.cinemabooking.repositories.ReservedSeatRepository;
+import hu.elte.cinemabooking.security.AuthenticatedUser;
 
-@Secured({"ROLE_ADMIN"})
+@Secured({"ROLE_ADMIN","ROLE_USER"})
 @RestController
 @RequestMapping("/bookings")
 public class BookingController {
@@ -18,8 +28,93 @@ public class BookingController {
     @Autowired
     private BookingRepository bookingRepository;
     
+    @Autowired
+    private ReservedSeatRepository reservedSeatRepository;
+    
+    @Autowired
+    private AuthenticatedUser authenticatedUser;
+    
     @GetMapping("")
 	public ResponseEntity<Iterable<Booking>> getAll() {
-		return ResponseEntity.ok(bookingRepository.findAll());
+    	User user = authenticatedUser.getUser();
+    	User.Role role = user.getRole();
+    	
+    	if(role.equals(User.Role.ROLE_ADMIN)) {
+    		return ResponseEntity.ok(bookingRepository.findAll());
+    	} else {
+    		return ResponseEntity.ok(bookingRepository.findAllByUser(user));
+    	}
 	}
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Booking> get(@PathVariable Integer id) {
+    	Optional<Booking> booking = bookingRepository.findById(id);
+    	User user = authenticatedUser.getUser();
+    	User.Role role = user.getRole();
+    	
+    	if(booking.isPresent() &&
+    			(user.getUsername().equals(booking.get().getUser().getUsername()) ||
+    					role.equals(User.Role.ROLE_ADMIN))) {
+    		return ResponseEntity.ok(booking.get());
+    	} else {
+    		return ResponseEntity.notFound().build();
+    	}
+    }
+    
+    @PostMapping("")
+    public ResponseEntity<Booking> post(@RequestBody Booking booking) {
+    	User user = authenticatedUser.getUser();
+    	booking.setUser(user);
+    	Booking savedBooking = bookingRepository.save(booking);
+    	return ResponseEntity.ok(savedBooking);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Booking> delete(@PathVariable Integer id) {
+    	Optional<Booking> oBooking = bookingRepository.findById(id);
+    	User user = authenticatedUser.getUser();
+    	User.Role role = user.getRole();
+    	
+    	if(oBooking.isPresent() &&
+    			(user.getUsername().equals(oBooking.get().getUser().getUsername()) ||
+    					role.equals(User.Role.ROLE_ADMIN))) {
+    		bookingRepository.deleteById(id);
+    		return ResponseEntity.ok().build();
+    	} else {
+    		return ResponseEntity.notFound().build();
+    	}
+    }
+    
+    @GetMapping("/{id}/reservedseats")
+    public ResponseEntity<Iterable<ReservedSeat>> seats(@PathVariable Integer id) {
+    	Optional<Booking> oBooking = bookingRepository.findById(id);
+    	User user = authenticatedUser.getUser();
+    	User.Role role = user.getRole();
+    	
+    	if(oBooking.isPresent() &&
+    			(user.getUsername().equals(oBooking.get().getUser().getUsername()) ||
+    					role.equals(User.Role.ROLE_ADMIN))) {
+    		return ResponseEntity.ok(oBooking.get().getReservedSeats());
+    	} else {
+    		return ResponseEntity.notFound().build();
+    	}
+    }
+    
+    @PostMapping("/{id}/reservedseats")
+    public ResponseEntity<ReservedSeat> insertSeats(@PathVariable Integer id, @RequestBody ReservedSeat reservedseat) {
+    	Optional<Booking> oBooking = bookingRepository.findById(id);
+    	User user = authenticatedUser.getUser();
+    	
+    	if(oBooking.isPresent() &&
+    			user.getUsername().equals(oBooking.get().getUser().getUsername())) {
+    		Booking booking = oBooking.get();
+    		reservedseat.setBooking(booking);
+    		return ResponseEntity.ok(reservedSeatRepository.save(reservedseat));
+    	} else {
+    		return ResponseEntity.notFound().build();
+    	}
+    }
+    
+    
+    
 }
--- cinema-booking/src/main/java/hu/elte/cinemabooking/repositories/BookingRepository.java
+++ cinema-booking/src/main/java/hu/elte/cinemabooking/repositories/BookingRepository.java
@@ -1,10 +1,14 @@
 package hu.elte.cinemabooking.repositories;
 
 import hu.elte.cinemabooking.entities.Booking;
+import hu.elte.cinemabooking.entities.User;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Integer>{
+
+	public Iterable<Booking> findAllByUser(User user);
     
 }
