84af0cc74af13ce55f14eca649cb405657ed75f5
starlord0302 <starlord0302@users.noreply.github.com>
2018-11-05 00:02:16 +0100

    ScreeningController updated with POST, PUT and DELETE http methods.

--- cinema-booking/src/main/java/hu/elte/cinemabooking/controllers/ScreeningController.java
+++ cinema-booking/src/main/java/hu/elte/cinemabooking/controllers/ScreeningController.java
@@ -1,8 +1,16 @@
 package hu.elte.cinemabooking.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +29,33 @@ public class ScreeningController {
 		return ResponseEntity.ok(screeningRepository.findAll());
 	}
 	
+	@Secured({"ROLE_ADMIN"})
+	@PostMapping("")
+	public ResponseEntity<Screening> post(@RequestBody Screening screening) {
+		return ResponseEntity.ok(screeningRepository.save(screening));
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@PutMapping("/{id}")
+	public ResponseEntity<Screening> update(@PathVariable Integer id, @RequestBody Screening screening) {
+		Optional<Screening> oScreening = screeningRepository.findById(id);
+		if(oScreening.isPresent()) {
+			screening.setId(id);
+			return ResponseEntity.ok(screeningRepository.save(screening));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@DeleteMapping("{/id}")
+	public ResponseEntity<Screening> delete(@PathVariable Integer id) {
+		Optional<Screening> oScreening = screeningRepository.findById(id);
+		if(oScreening.isPresent()) {
+			screeningRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
