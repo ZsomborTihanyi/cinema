ddb49f98101a9e1da9ce847e1a2f5c7eec702f4b
starlord0302 <starlord0302@users.noreply.github.com>
2018-11-05 00:38:28 +0100

    UserController updated with GET http method.

--- cinema-booking/src/main/java/hu/elte/cinemabooking/controllers/UserController.java
+++ cinema-booking/src/main/java/hu/elte/cinemabooking/controllers/UserController.java
@@ -4,12 +4,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hu.elte.cinemabooking.entities.Movie;
 import hu.elte.cinemabooking.entities.User;
 import hu.elte.cinemabooking.repositories.UserRepository;
 import hu.elte.cinemabooking.security.AuthenticatedUser;
@@ -39,5 +43,22 @@ public class UserController {
     @PostMapping("login")
     public ResponseEntity login(@RequestBody User user) {
         return ResponseEntity.ok().build();
+    }
+    
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    @GetMapping("")
+	public ResponseEntity<Iterable<User>> getAll() {
+		return ResponseEntity.ok(userRepository.findAll());
+	}
+    
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    @GetMapping("{/id}/movies")
+    public ResponseEntity<Iterable<Movie>> movies(@PathVariable Long id) {
+    	Optional<User> oUser = userRepository.findById(id);
+    	if(oUser.isPresent()) {
+    		return ResponseEntity.ok(oUser.get().getMovies());
+    	} else {
+    		return ResponseEntity.notFound().build();
+    	}
     }
 }
