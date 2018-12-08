0606199f6b32069e36c8e0f5331f652b5a5c7a7b
starlord0302 <starlord0302@users.noreply.github.com>
2018-12-08 13:17:20 +0100

    Genre-filter component added

--- cinema-booking-frontend/src/app/app.module.ts
+++ cinema-booking-frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  MatButtonToggleModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
 import { HttpClientModule } from "@angular/common/http";
+import { GenreFilterComponent } from './components/movies/genre-filter/genre-filter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    GenreFilterComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cinema-booking-frontend/src/app/components/movies/genre-filter/genre-filter.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'genre-filter',
+  templateUrl: './genre-filter.component.html',
+  styleUrls: ['./genre-filter.component.css']
+})
+export class GenreFilterComponent implements OnInit {
+
+  @Input('genre') selectedGenre = 'DOING'
+  @Output() change = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onFilterChange(data) {
+    this.selectedGenre = data.value;
+    this.change.emit(this.selectedGenre);
+  }
+}
